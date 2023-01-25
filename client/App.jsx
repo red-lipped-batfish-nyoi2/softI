cb11d8919475df0b7959189253a812a48887053a
Nancy Zhang <zhangn356@gmail.com>
2023-01-25 15:06:20 -0500

    frontend login logic

--- client/App.jsx
+++ client/App.jsx
@@ -15,7 +15,6 @@ function App() {
 
   return (
     <div id='main'>
-      //define routes
       <Routes>
         {/* //default route is the login screen */}
         <Route path='/' element={<Login />} />
